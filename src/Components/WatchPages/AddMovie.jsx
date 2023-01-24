6a16938475e8f4095c75232007f43ee689b4ce2d
trba1810 <trba8l@live.com>
2023-01-24 10:55:54 +0100

    finished add movie component

--- src/Components/WatchPages/AddMovie.jsx
+++ src/Components/WatchPages/AddMovie.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
-const AddMovie = () => {
+const AddMovie = (props) => {
+  const [name, setName] = useState();
+
+  function submitAddMovieForm(e, props) {
+    e.preventDefault();
+    props.handleAddMovie(name);
+    setName("");
+  }
+
   return (
-    <form>
+    <form onSubmit={(e) => submitAddMovieForm(e, props)}>
       <div className="row text-white">
         <div className="col-12 text-center py-1 h4 text-success">
           Dodaj Film
@@ -12,6 +21,8 @@ const AddMovie = () => {
             type="text"
             className="form-control"
             placeholder="Ime Filma..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           ></input>
         </div>
         <div className="col-2">
--- src/Components/WatchPages/MoviePage.jsx
+++ src/Components/WatchPages/MoviePage.jsx
@@ -7,9 +7,18 @@ const MoviePage = () => {
   const [movieState, setMovieState] = useState(() => {
     return { movieList: ["Harry Potter", "Goblin"] };
   });
+
+  function handleAddMovie(newMovie) {
+    setMovieState((prevState) => {
+      return {
+        movieList: prevState.movieList.concat(newMovie),
+      };
+    });
+  }
+
   return (
     <div className="container col-12 col-md-6 my-3 border">
-      <AddMovie />
+      <AddMovie handleAddMovie={handleAddMovie} />
       <MovieList movieList={movieState.movieList} />
     </div>
   );
