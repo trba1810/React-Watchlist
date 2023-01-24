e8cd48e5e7ee9dc663b2483404edf3e250a6f987
trba1810 <trba8l@live.com>
2023-01-24 09:46:59 +0100

    import hooks

--- src/Components/WatchPages/Counter.jsx
+++ src/Components/WatchPages/Counter.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useState } from "react";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(2);
   return (
     <div className="col-12 col-md-4 offset-md-4 border text-white">
       <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span> <br />
@@ -8,7 +10,7 @@ const Counter = () => {
       <button className="btn btn-danger m-1">-1</button> <br />
       <span className="h4">
         Counter: &nbsp;
-        <span className="text-success">0</span>
+        <span className="text-success">{counter}</span>
       </span>
     </div>
   );
