75c86233bdfcf5f7d29a13efe81e9e27f18a515b
trba1810 <trba8l@live.com>
2023-01-24 10:59:44 +0100

    change names

--- src/Components/WatchPages/Counter.jsx
+++ src/Components/WatchPages/Counter.jsx
@@ -5,7 +5,7 @@ const Counter = () => {
   const [counterState, setCounterState] = useState(() => {
     return {
       counter: 0,
-      title: "Fun",
+      title: "Brojac",
     };
   });
   function incrementCounter() {
@@ -20,9 +20,7 @@ const Counter = () => {
   }
   return (
     <div className="col-12 col-md-4 offset-md-4 border text-white">
-      <span className="h2 pt-4 m-2 text-white-50">
-        {counterState.title} Counter
-      </span>{" "}
+      <span className="h2 pt-4 m-2 text-white-50">{counterState.title}</span>{" "}
       <br />
       <button className="btn btn-success m-1" onClick={incrementCounter}>
         +1
@@ -32,7 +30,7 @@ const Counter = () => {
       </button>{" "}
       <br />
       <span className="h4">
-        Counter: &nbsp;
+        Stanje: &nbsp;
         <span className="text-success">{counterState.counter}</span>
       </span>
     </div>
